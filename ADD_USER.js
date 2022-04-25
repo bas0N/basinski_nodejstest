e1c798d913757b6c00c1a2f121d051f57a0340cf
Wojciech Basinski <86808206+bas0N@users.noreply.github.com>
2022-04-25 09:31:58 +0200

    Update ADD_USER.js

--- ADD_USER.js
+++ ADD_USER.js
@@ -1,27 +1,30 @@
 const AWS = require("aws-sdk");
-
+//connecting to db
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-  let requestJSON =  JSON.parse(JSON.stringify(event.body))
-  let id=await AWS.util.uuid.v4();
-  if(!requestJSON.firstName||!requestJSON.age){
-    return{statusCode:400,body: 'Incorrect body!'}
+  //parsing body request
+  let requestJSON = JSON.parse(JSON.stringify(event.body));
+  //generating random uuid
+  let id = await AWS.util.uuid.v4();
+  //request parameters error handling
+  if (!requestJSON.firstName || !requestJSON.age) {
+    return { statusCode: 400, body: "Incorrect body!" };
   }
+  //creating params object to be added
   var params = {
-  TableName: 'users',
-  Item: {
-    'user_id':id,
-    'firstName' :requestJSON.firstName,
-    'age' : requestJSON.age,
-  }
-};
+    TableName: "users",
+    Item: {
+      user_id: id,
+      firstName: requestJSON.firstName,
+      age: requestJSON.age,
+    },
+  };
+  //adding element to dynamodb
   try {
-   await dynamo.put(params).promise();
-    return { body: 'Successfully created item!',
-    id:id};
+    await dynamo.put(params).promise();
+    return { body: "Successfully created item!", id: id };
   } catch (err) {
     return { error: err };
   }
 };
-
